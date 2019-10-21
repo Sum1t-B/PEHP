4004951427b57df124c9788a94b9eaf658437f64
Sum1t-B <sumitbrnwl48@gmail.com>
2019-10-21 19:41:07 +0530

    Safer way of executing the same script

--- mac_changer.py
+++ mac_changer.py
@@ -3,7 +3,7 @@
 import subprocess
 import optparse
 
-
+# # Uncomment this block before using script
 # subprocess.call("date >> ~/Desktop/pehp/comdlog.txt", shell=True)
 # subprocess.call("ifconfig >> ~/Desktop/pehp/comdlog.txt", shell=True)
 
@@ -22,6 +22,15 @@ new_mac = options.new_mac
 
 print("[+] Changing mac address for " + interface + " to "+ new_mac)
 
+# ##################
+# # Not very safe way of execution of system commands while using user input
 # subprocess.call("ifconfig "+ interface+" down", shell=True)
 # subprocess.call("ifconfig "+ interface+" hw ether " + new_mac, shell=True)
 # subprocess.call("ifconfig "+ interface+" up", shell=True)
+
+# # Uncomment this block before using script
+# ####################
+# # Safer way of executing system commands while using user input
+# subprocess.call(["ifconfig", interface,"down"])
+# subprocess.call(["ifconfig", interface, "hw ether", new_mac])
+# subprocess.call(["ifconfig", interface, "up"])
