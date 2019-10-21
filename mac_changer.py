e3b458e89abe75cb3b3a5f7ed5f6cbee5eb193cc
Sum1t-B <sumitbrnwl48@gmail.com>
2019-10-21 19:57:30 +0530

    putting parsing and mac changing actions into functions

--- mac_changer.py
+++ mac_changer.py
@@ -7,30 +7,32 @@ import optparse
 # subprocess.call("date >> ~/Desktop/pehp/comdlog.txt", shell=True)
 # subprocess.call("ifconfig >> ~/Desktop/pehp/comdlog.txt", shell=True)
 
-#######################
-# code from here
+################################
+##########
+# Main code from here
 
-parser = optparse.OptionParser()
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i","--interface", dest="interface", help=" Interface to change mac address")
+    parser.add_option("-m","--mac", dest="new_mac", help=" new mac address")
+    return parser.parse_args()
 
-parser.add_option("-i","--interface", dest="interface", help=" Interface to change mac address")
-parser.add_option("-m","--mac", dest="new_mac", help=" new mac address")
+def change_mac(interface,new_mac):
+    print("[+] Changing mac address for " + interface + " to "+ new_mac)
+    # ##################
+    # # Not very safe way of execution of system commands while using user input
+    # subprocess.call("ifconfig "+ interface+" down", shell=True)
+    # subprocess.call("ifconfig "+ interface+" hw ether " + new_mac, shell=True)
+    # subprocess.call("ifconfig "+ interface+" up", shell=True)
 
-(options, arguments) = parser.parse_args()
+    # # Uncomment this block before using script
+    # ####################
+    # # Safer way of executing system commands while using user input
+    # subprocess.call(["ifconfig", interface,"down"])
+    # subprocess.call(["ifconfig", interface, "hw ether", new_mac])
+    # subprocess.call(["ifconfig", interface, "up"])
 
-interface = options.interface
-new_mac = options.new_mac
 
-print("[+] Changing mac address for " + interface + " to "+ new_mac)
 
-# ##################
-# # Not very safe way of execution of system commands while using user input
-# subprocess.call("ifconfig "+ interface+" down", shell=True)
-# subprocess.call("ifconfig "+ interface+" hw ether " + new_mac, shell=True)
-# subprocess.call("ifconfig "+ interface+" up", shell=True)
-
-# # Uncomment this block before using script
-# ####################
-# # Safer way of executing system commands while using user input
-# subprocess.call(["ifconfig", interface,"down"])
-# subprocess.call(["ifconfig", interface, "hw ether", new_mac])
-# subprocess.call(["ifconfig", interface, "up"])
+(options, arguments) = get_arguments()
+change_mac(options.interface, options.new_mac)
