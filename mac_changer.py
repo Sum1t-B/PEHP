8ebcafcf761780442668223e6b33818d1a9c9cc5
Sum1t-B <sumitbrnwl48@gmail.com>
2019-10-21 19:23:56 +0530

    can take cl arguments as -i & -m and parse them as variables

--- mac_changer.py
+++ mac_changer.py
@@ -4,20 +4,24 @@ import subprocess
 import optparse
 
 
-subprocess.call("date >> ~/Desktop/pehp/comdlog.txt", shell=True)
-subprocess.call("ifconfig >> ~/Desktop/pehp/comdlog.txt", shell=True)
+# subprocess.call("date >> ~/Desktop/pehp/comdlog.txt", shell=True)
+# subprocess.call("ifconfig >> ~/Desktop/pehp/comdlog.txt", shell=True)
 
 #######################
 # code from here
 
 parser = optparse.OptionParser()
 
+parser.add_option("-i","--interface", dest="interface", help=" Interface to change mac address")
+parser.add_option("-m","--mac", dest="new_mac", help=" new mac address")
 
-interface = input("interface > ")
-new_mac = input("new_mac > ")
+(options, arguments) = parser.parse_args()
+
+interface = options.interface
+new_mac = options.new_mac
 
 print("[+] Changing mac address for " + interface + " to "+ new_mac)
 
-subprocess.call("ifconfig "+ interface+" down", shell=True)
-subprocess.call("ifconfig "+ interface+" hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig "+ interface+" up", shell=True)
+# subprocess.call("ifconfig "+ interface+" down", shell=True)
+# subprocess.call("ifconfig "+ interface+" hw ether " + new_mac, shell=True)
+# subprocess.call("ifconfig "+ interface+" up", shell=True)
