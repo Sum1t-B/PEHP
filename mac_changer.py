91cc6fcb0d5f452a6baf55e9b235034335e4411f
Sum1t-B <sumitbrnwl48@gmail.com>
2019-10-26 17:01:37 +0530

    log capability added

--- mac_changer.py
+++ mac_changer.py
@@ -9,6 +9,13 @@ from datetime import datetime  as dt
 ################################
 ##########
 # Methods and functions definitions here
+def log(logstring):
+    # To log changes
+    log_file = open("./log.txt","a+")
+    L = [dt.now().strftime("%Y-%m-%d %H:%M:%S")+"   ", logstring, "\n"]
+    log_file.writelines(L)
+    log_file.close()
+
 def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i","--interface", dest="interface", help=" Interface to change mac address")
@@ -30,14 +37,8 @@ def change_mac(interface,new_mac):
     # subprocess.call("ifconfig "+ interface+" hw ether " + new_mac, shell=True)
     # subprocess.call("ifconfig "+ interface+" up", shell=True)
 
-    # Uncomment this block before using script
-    #### To keep track of mac address changes
-    log_file = open("./comd_log.txt","a+")
-    L = [dt.now().strftime("%Y-%m-%d %H:%M:%S")+"\n", interface + ": " + str(get_current_mac(interface)), "\n \n"]
-    log_file.writelines(L)
-    log_file.close()
-    # subprocess.call("date >> ~/Desktop/pehp/comdlog.txt")
-    # subprocess.call(["echo", interface, str(get_current_mac(interface)), ">>","~/Desktop/pehp/comdlog.txt"])
+    # saving the current mac address to log
+    log(interface + ": " + str(get_current_mac(interface)))
 
     # Uncomment this block before using script
     ####################
@@ -50,8 +51,10 @@ def change_mac(interface,new_mac):
     current_mac = get_current_mac(interface)
     if current_mac == new_mac:
         print("[+] Mac address was successfully changed to "+ current_mac)
+        log(interface + ": mac address changed to "+ current_mac)
     else:
         print("[-] Mac address did not get changed.")
+        log(interface + ": mac address change failed")
 
 def get_current_mac(interface):
     ifconfig_result = subprocess.check_output(["ifconfig", interface])
@@ -66,10 +69,10 @@ def get_current_mac(interface):
 ################################
 ##########
 # Main Code here
-# try:
-options = get_arguments()
-current_mac = get_current_mac(options.interface)
-print("Current Mac = " + str(current_mac))
-change_mac(options.interface, options.new_mac)
-# except :
-#     print("No such interface. Enter a valid Interface. Run ifconfig or equivalent command to know about available interfaces.")
+try:
+    options = get_arguments()
+    current_mac = get_current_mac(options.interface)
+    print("Current Mac = " + str(current_mac))
+    change_mac(options.interface, options.new_mac)
+except :
+    print("One or more errors occured.")
